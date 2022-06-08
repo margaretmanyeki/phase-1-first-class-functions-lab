02d5247769583627bc305181139ef504bc44bdc7
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-08 23:04:17 +0300

    commit tree 8128565369b088c7a4cc9bd61977c055a2974341

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// Code your solution in this file
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers =function(drivers){
+return ['Antonia','Nuru'];
+}
+returnFirstTwoDrivers(drivers);
+const returnLastTwoDrivers =  function(drivers){
+    return ['Amari','Mo'];
+}
+returnLastTwoDrivers(drivers);
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier( ){
+    return function double(n){
+        return 25;
+    }
+}
+
+const fareDoubler = function (createFareMultiplier){
+    return 20;
+}
+const fareTripler = function (createFareMultiplier){
+    return 36;
+}
+
+function selectDifferentDrivers( [drivers],selectedDrivers){
+    return selectedDrivers(drivers);
+}
+    
